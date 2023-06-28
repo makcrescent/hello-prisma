d049f7c9e3ae556eb54bbf0602a8888683a13439
Muhammad Aslam Khan <makcrescent@gmail.com>
2023-06-28 18:47:41 +0530

    committing prisma basic project

---
+++ script.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Aslam Khan',
+            email: 'alice@gmail.com',
+        },
+    })
+    const users = await prisma.user.findMany()
+    console.log(users)
+}
+main().then(async () => {
+    await prisma.$disconnect()
+})
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
